803dc78a13a12bf7ee55a9b78a1ab0dac6822471
cakethebake2 <cakethebake2@gmail.com>
2026-07-15 18:46:19 -0700

    Add PlayerESP module

---
+++ src/main/java/com/example/addon/modules/PlayerESP.java
@@ -0,0 +1,9 @@
+package com.example.addon.modules;
+
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class PlayerESP extends Module {
+    public PlayerESP() {
+        super("Player ESP", "Highlights other players through walls");
+    }
+}
