a9a31a2c42bf91e7cd511a799e073111c2e3f03d
cakethebake2 <cakethebake2@gmail.com>
2026-07-15 19:04:44 -0700

    Fix: OreESP module constructor

--- src/main/java/com/example/addon/modules/OreESP.java
+++ src/main/java/com/example/addon/modules/OreESP.java
@@ -1,9 +1,10 @@
 package com.example.addon.modules;
 
 import meteordevelopment.meteorclient.systems.modules.Module;
+import com.example.addon.AddonTemplate;
 
 public class OreESP extends Module {
     public OreESP() {
-        super("Ore ESP", "Highlights ores through walls");
+        super(AddonTemplate.CATEGORY, "Ore ESP", "Highlights ores through walls");
     }
 }
