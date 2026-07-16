195664773d0ee280f1b00d8d8e28e455d9f32071
cakethebake2 <cakethebake2@gmail.com>
2026-07-15 17:04:24 -0700

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,35 +1,31 @@
 package com.example.addon;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
-import com.mojang.logging.LogUtils;
-import meteordevelopment.meteorclient.addons.GithubRepo;
+import com.example.addon.modules.NoClip;
+import com.example.addon.modules.OreESP;
+import com.example.addon.modules.PlayerESP;
+import com.example.addon.modules.SpawnerFinder;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AddonTemplate extends MeteorAddon {
-    public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+    // Logger, use this instead of System.out.println()
+    public static final Logger LOG = LogManager.getLogger("SpawnerUtils");
+
+    // Custom category shown in the module list / search
+    public static final Category CATEGORY = new Category("SpawnerUtils");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing SpawnerUtils");
 
-        // Modules
-        Modules.get().add(new ModuleExample());
-
-        // Commands
-        Commands.add(new CommandExample());
-
-        // HUD
-        Hud.get().register(HudExample.INFO);
+        // Register modules here
+        Modules.get().add(new SpawnerFinder());
+        Modules.get().add(new NoClip());
+        Modules.get().add(new OreESP());
+        Modules.get().add(new PlayerESP());
     }
 
     @Override
@@ -40,10 +36,5 @@ public class AddonTemplate extends MeteorAddon {
     @Override
     public String getPackage() {
         return "com.example.addon";
-    }
-
-    @Override
-    public GithubRepo getRepo() {
-        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
     }
 }
