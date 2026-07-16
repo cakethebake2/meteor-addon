f421157a6402cc7c45b5527a359d2bee64f5351c
cakethebake2 <cakethebake2@gmail.com>
2026-07-15 18:46:44 -0700

    Add OreESP module

---
+++ src/main/java/com/example/addon/modules/OreESP.java
@@ -0,0 +1,9 @@
+package com.example.addon.modules;
+
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class OreESP extends Module {
+    public OreESP() {
+        super("Ore ESP", "Highlights ores through walls");
+    }
+}
