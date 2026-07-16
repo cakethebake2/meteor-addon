a0a48a9faec06e0dd3699e275c12bc3e5da36f52
cakethebake2 <cakethebake2@gmail.com>
2026-07-15 19:05:58 -0700

    Fix: SpawnerFinder module constructor with Category

--- src/main/java/com/example/addon/modules/SpawnerFinder.java
+++ src/main/java/com/example/addon/modules/SpawnerFinder.java
@@ -1,9 +1,10 @@
 package com.example.addon.modules;
 
 import meteordevelopment.meteorclient.systems.modules.Module;
+import com.example.addon.AddonTemplate;
 
 public class SpawnerFinder extends Module {
     public SpawnerFinder() {
-        super("Spawner Finder", "Highlights mob spawners in the world");
+        super(AddonTemplate.CATEGORY, "Spawner Finder", "Highlights mob spawners in the world");
     }
 }
