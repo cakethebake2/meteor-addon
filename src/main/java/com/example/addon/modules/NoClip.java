42fd083b7d84b7f0ebc7d455ad0a126e3fc02d48
cakethebake2 <cakethebake2@gmail.com>
2026-07-15 18:46:52 -0700

    Add NoClip module

---
+++ src/main/java/com/example/addon/modules/NoClip.java
@@ -0,0 +1,9 @@
+package com.example.addon.modules;
+
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class NoClip extends Module {
+    public NoClip() {
+        super("NoClip", "Walk through blocks");
+    }
+}
