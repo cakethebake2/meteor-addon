e11fef9631124d6a29b5e3e39a54b9a70321ab39
cakethebake2 <cakethebake2@gmail.com>
2026-07-15 19:07:32 -0700

    Disable: ExampleMixin - causing compilation errors

--- src/main/java/com/example/addon/mixin/ExampleMixin.java
+++ src/main/java/com/example/addon/mixin/ExampleMixin.java
@@ -1,19 +1,2 @@
-package com.example.addon.mixin;
-
-import net.minecraft.entity.Entity;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
-
-/**
- * Entity#noClip is protected and normally only true for entities in
- * spectator mode. This accessor lets modules flip it on the player entity
- * directly, which is how NoClip/"Debug" style movement modules work.
- */
-@Mixin(Entity.class)
-public interface EntityAccessor {
-    @Accessor("noClip")
-    void meteor$setNoClip(boolean noClip);
-
-    @Accessor("noClip")
-    boolean meteor$getNoClip();
-}
+// This file is disabled and should be deleted
+// Example mixin files require proper setup - delete this file to resolve compilation errors
