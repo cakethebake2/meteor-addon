088c26f20b944f4335e9e1c1df7aae84bfa52c2a
cakethebake2 <cakethebake2@gmail.com>
2026-07-15 18:47:00 -0700

    Add SpawnerFinder module

---
+++ src/main/java/com/example/addon/modules/SpawnerFinder.java
@@ -0,0 +1,9 @@
+package com.example.addon.modules;
+
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class SpawnerFinder extends Module {
+    public SpawnerFinder() {
+        super("Spawner Finder", "Highlights mob spawners in the world");
+    }
+}
